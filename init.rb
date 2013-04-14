864aa74bf5ccb8e38fca556213bfde7a1704b7ae
Rudiney Altair Franceschi <rudi.atp@gmail.com>
2013-04-14 09:38:12 -0300

    agora pode ser configurado para qual projeto deseja habilitar o relatório de OS

--- init.rb
+++ init.rb
@@ -7,7 +7,9 @@ Redmine::Plugin.register :questor_relatorio_os do
 	version '0.0.1'
 	url 'http://www.questor.com.br'
 	author_url 'mailto:rudiney@oren.net.br'
-
-	permission :relatorio_os, { :relatorio_os => [:index, :show] }, :public => true
+	
+	project_module :relatorio_os do
+		permission :relatorio_os, { :relatorio_os => [:index, :show] }, :public => true
+	end
 	menu :project_menu, :relatorio_os, { :controller => 'relatorio_os', :action => 'index' }, :caption => 'Relatório de OS', :after => :activity, :param => :project_id
 end
